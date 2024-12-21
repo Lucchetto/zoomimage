f934398c5d230b2b78f79f026df2af92d2bc50cf
Lucchetto <lucchetto.tie@live.com>
2024-12-21 11:26:32 +0100

    Improve comparison
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- sample/src/commonMain/kotlin/com/github/panpf/zoomimage/sample/ui/App.kt
+++ sample/src/commonMain/kotlin/com/github/panpf/zoomimage/sample/ui/App.kt
@@ -1,44 +1,32 @@
 package com.github.panpf.zoomimage.sample.ui
 
+import androidx.annotation.FloatRange
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.transitions.ScaleTransition
 import com.github.panpf.sketch.fetch.newComposeResourceUri
 import com.github.panpf.zoomimage.CoilZoomAsyncImage
 import com.github.panpf.zoomimage.CoilZoomState
-import com.github.panpf.zoomimage.compose.ZoomState
 import com.github.panpf.zoomimage.compose.coil.CoilComposeSubsamplingImageGenerator
 import com.github.panpf.zoomimage.compose.rememberZoomImageLogger
-import com.github.panpf.zoomimage.compose.rememberZoomState
 import com.github.panpf.zoomimage.compose.subsampling.rememberSubsamplingState
 import com.github.panpf.zoomimage.compose.zoom.ZoomableState
 import com.github.panpf.zoomimage.compose.zoom.rememberProxyZoomableState
 import com.github.panpf.zoomimage.compose.zoom.rememberZoomableState
-import com.github.panpf.zoomimage.rememberCoilZoomState
-import com.github.panpf.zoomimage.sample.EventBus
 import com.github.panpf.zoomimage.sample.resources.Res
 import com.github.panpf.zoomimage.sample.ui.theme.AppTheme
-import com.github.panpf.zoomimage.sample.util.Platform
-import com.github.panpf.zoomimage.sample.util.current
-import com.github.panpf.zoomimage.sample.util.isMobile
 import com.github.panpf.zoomimage.util.Logger
 import com.github.panpf.zoomimage.util.Logger.Level
 import kotlinx.collections.immutable.ImmutableList
@@ -57,45 +45,66 @@ fun rememberCoilZoomState(
     }
 }
 
+@Immutable
+private data class ClipWidthShape(
+    @FloatRange(from = 0.0, to = 1.0) private val start: Float,
+    @FloatRange(from = 0.0, to = 1.0) private val end: Float
+): Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        val leftClipFraction: Float
+        val rightClipFraction: Float
+
+        when (layoutDirection) {
+            LayoutDirection.Ltr -> {
+                leftClipFraction = start
+                rightClipFraction = end
+            }
+            LayoutDirection.Rtl -> {
+                leftClipFraction = end
+                rightClipFraction = start
+            }
+        }
+
+        val rect = Rect(
+            left = size.width * leftClipFraction,
+            top = 0f,
+            right = size.width - size.width * rightClipFraction,
+            bottom = size.height
+        )
+
+        return Outline.Rectangle(rect)
+    }
+}
+
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App(onContentChanged: ((Navigator) -> Unit)? = null) {
     AppTheme {
         Scaffold {
-            BoxWithConstraints {
-                val parentMaxWidth = maxWidth
-                val parentMaxHeight = maxHeight
+            Box(modifier = Modifier.fillMaxSize()) {
                 val afterZoomableState = rememberZoomableState()
                 val beforeZoomableState = rememberProxyZoomableState(afterZoomableState)
 
                 val beforeZoomState = rememberCoilZoomState(beforeZoomableState)
                 val afterZoomState = rememberCoilZoomState(afterZoomableState)
 
-                Row(
-                    modifier = Modifier.fillMaxHeight()
-                ) {
-                    BoxWithConstraints(
-                        modifier = Modifier.weight(1f).clipToBounds()
-                    ) {
-                        CoilZoomAsyncImage(
-                            model = newComposeResourceUri(Res.getUri("files/pexels-taryn-elliott-4253835_400.jpg")),
-                            contentDescription = null,
-                            modifier = Modifier.padding(start = parentMaxWidth - maxWidth).requiredSize(parentMaxWidth, parentMaxHeight),
-                            zoomState = beforeZoomState,
-                        )
-                    }
-                    BoxWithConstraints(
-                        modifier = Modifier.weight(1f).clipToBounds()
-                    ) {
-                        CoilZoomAsyncImage(
-                            model = newComposeResourceUri(Res.getUri("files/pexels-taryn-elliott-4253835.jpg")),
-                            contentDescription = null,
-                            modifier = Modifier.padding(end = parentMaxWidth - maxWidth).requiredSize(parentMaxWidth, parentMaxHeight),
-                            zoomState = afterZoomState,
-                        )
-                    }
+                CoilZoomAsyncImage(
+                    model = newComposeResourceUri(Res.getUri("files/pexels-taryn-elliott-4253835_400.jpg")),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize().clip(ClipWidthShape(0f, 0.5f)),
+                    zoomState = beforeZoomState,
+                )
 
-                }
+                CoilZoomAsyncImage(
+                    model = newComposeResourceUri(Res.getUri("files/pexels-taryn-elliott-4253835.jpg")),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize().clip(ClipWidthShape(0.5f, 0f)),
+                    zoomState = afterZoomState,
+                )
             }
         }
     }
